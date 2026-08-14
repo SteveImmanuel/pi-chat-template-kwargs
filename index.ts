d01047c0b75a479a09bd4076fb78d2545f178abd
SteveImmanuel <steveandreasimanuel@gmail.com>
2026-08-14 13:48:26 +0900

    Project init

---
+++ index.ts
@@ -0,0 +1,5 @@
+import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+
+export default function (pi: ExtensionAPI) {
+	// TODO: /chat-template command + before_provider_request hook
+}
